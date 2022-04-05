cf0139f628249eadb70d6476e5e5f111c34849c3
ItsTarik <tarik.lefi@gmail.com>
2022-04-05 03:45:33 +0100

    ✨ FEAT: test OG with maserati|tw summary_large_image

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,8 @@ const SocialHead = ({ title, description, previewImage }) => {
       />
 
       {/* Twitter */}
-      <meta name="twitter:card" content="summary" key="twcard" />
+      {/* <meta name="twitter:card" content="summary" key="twcard" /> */}
+      <meta name="twitter:card" content="summary_large_image" key="twcard" />
       <meta name="twitter:creator" content={"twitterHandle"} key="twhandle" />
       <meta name="twitter:image" content={previewImage} key="twImage" />
     </Head>
